fb189435724eace215a63b8660cbe9f9ce134e0d
piku0709 <piku.210@gmail.com>
2023-10-09 19:28:14 -0400

    added some meaningful contents to each component except recipe-item

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/0/0d/Indian_Recipes_Gajar_Ka_Halwa_A_Dessert_Recipe_from_Indian_Recipes_By_Sonia_Goyal.jpg'),
+    new Recipe('A Test recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/0/0d/Indian_Recipes_Gajar_Ka_Halwa_A_Dessert_Recipe_from_Indian_Recipes_By_Sonia_Goyal.jpg')
+  ]
 
+  constructor(){  }
+
+  ngOnInit(){  }
 }
